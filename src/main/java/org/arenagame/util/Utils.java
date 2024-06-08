1ccb6d7df1e690a08d9396a2648a3794d4821d11
skumar25 <skumar25@atlassian.com>
2024-06-08 18:19:34 +0530

    made some changes to utils and pushed intellij project settings

--- src/main/java/org/arenagame/util/Utils.java
+++ src/main/java/org/arenagame/util/Utils.java
@@ -13,8 +13,8 @@ public class Utils {
     }
     public static Scanner scanner = new Scanner(System.in);
 
-    private static String inputStringFromUser(String prompt) {
-        System.out.print(prompt);
+    private static String inputStringFromUser() {
+        System.out.print("Enter the player's name: ");
         return scanner.nextLine();
     }
 
@@ -30,7 +30,7 @@ public class Utils {
     public static Player inputNewPlayerDetails() {
         Random random = new Random();
         int id = random.nextInt(Integer.MAX_VALUE);
-        String name = inputStringFromUser("Enter the player's name: ");
+        String name = inputStringFromUser();
         int health = inputIntegerFromUser(String.format("Enter %s's health: ", name));
         int attack = inputIntegerFromUser(String.format("Enter %s's attack: ", name));
         int strength = inputIntegerFromUser(String.format("Enter %s's strength: ", name));
